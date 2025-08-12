7b03226dfcb1e7772f2b759d4e69dbab43ae3dcf
TrAyesha123 <qaziayesha966@gmail.com>
2025-08-12 12:03:16 +0500

    WorkBeyondSection

--- src/Pages/HomePage/HomePage.jsx
+++ src/Pages/HomePage/HomePage.jsx
@@ -1,24 +1,24 @@
-import React from 'react'
-import Mid_Cards from './../../Component/Mid_Cards/MidCards'
-import Innovation from '../../Component/Innovation/Innovation'
-import Hero from '../Hero/Hero'
-import Faq from '../Faq/Faq'
-import About from '../About/About'
-import Awards from '../../Component/Awards/Awards'
+import React from "react";
+import Mid_Cards from "./../../Component/Mid_Cards/MidCards";
+import Innovation from "../../Component/Innovation/Innovation";
+import Hero from "../Hero/Hero";
+import Faq from "../Faq/Faq";
+import About from "../About/About";
+import Awards from "../../Component/Awards/Awards";
 import JourneyBeyondWork from "../../Component/JourneyBeyondWork/JourneyBeyond";
 const HomePage = () => {
   return (
-    <div style={{width:"100%",background:"white",maxWidth:"1920px"}}>
-      {/* <Hero/>
+    <div style={{ width: "100%", background: "white", maxWidth: "1920px" }}>
+      <Hero />
 
-      <About/>
+      <About />
       <Mid_Cards />
-      <Innovation/>
-      <Awards/> */}
+      <Innovation />
+      <Awards />
       <JourneyBeyondWork />
-      <Faq/>
+      <Faq />
     </div>
-  )
-}
+  );
+};
 
-export default HomePage
+export default HomePage;
